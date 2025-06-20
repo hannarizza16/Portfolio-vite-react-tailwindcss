de5956b0621048db62134efac15249c45d5d4636
hannamalana98 <hannamalana98@gmail.com>
2025-06-20 14:51:49 +0800

    fixed the resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -8,9 +8,10 @@ export default function Resume() {
     {/* <div className="flex place-items-center justify-center min-w-[320px] min-h-[100vh] "> */}
       <button onClick={() => {
         const link = document.createElement("a")
-        link.href = "/public/Resume_Hanna_Malana.pdf"
+        link.href = "Resume_Hanna_Malana.pdf"
         // link.download = "Resume_Hanna_Rizza_Malana.pdf"
         link.target = "_blank"
+        link.rel="noopener noreferrer" // 
         link.click()
       }} className=" p-1 bg-yellow-200 text-black rounded-lg shadow hover:bg-yellow-300 transition cursor-pointer">
 
