be6693f6ce7bbd16fdf374e86e6e9fc0cac700cf
hannamalana98 <hannamalana98@gmail.com>
2025-07-14 02:20:59 +0800

    update resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -8,7 +8,7 @@ export default function Resume() {
     {/* <div className="flex place-items-center justify-center min-w-[320px] min-h-[100vh] "> */}
       <button onClick={() => {
         const link = document.createElement("a")
-        link.href = "Resume_Hanna_Malana.pdf"
+        link.href = "HannaMalana_Resume.pdf"
         // link.download = "Resume_Hanna_Rizza_Malana.pdf"
         link.target = "_blank"
         link.rel="noopener noreferrer" // 
